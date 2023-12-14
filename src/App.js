4bd4fc0c32e888422c28867a9f491db983facf12
Panks28 <pankajbhadwalinert@gmail.com>
2023-12-14 17:37:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import { Image } from "../public/Images/pexels-cottonbro-studio-6153354.jpg";
+
+const App = () => {
+  return (
+    <div>
+      <h2 id="heading">
+        Welcome to Custom React App Build from Scratch using Babel and Webpack!
+      </h2>
+      <img src="Images/pexels-cottonbro-studio-6153354.jpg"></img>{" "}
+    </div>
+  );
+};
+
+export default App;
